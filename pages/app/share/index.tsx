e67a85f678c7f0ec7a83bab49eaa65b066f9a9ff
Bård N. Hovde <bnhovde@gmail.com>
2023-09-26 20:07:41 +0200

    Allow copying of sprite date on share page

--- pages/app/share/index.tsx
+++ pages/app/share/index.tsx
@@ -18,7 +18,7 @@ const SpritePlayerLarge = dynamic(
 
 import { decodeUrlSprite, getRandomHash } from "utils/hash";
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import { Sprite, URLSprite } from "types/sprite";
 
@@ -54,8 +54,18 @@ const decompress = (data?: string | string[]): Sprite | null => {
 const Home: NextPage = () => {
   const router = useRouter();
   const { query } = router;
+  const [isCopied, setIsCopied] = useState(false);
 
   const spriteData = decompress(query.d);
+
+  const copyData = async (data?: string | string[]) => {
+    try {
+      await navigator.clipboard.writeText(String(data));
+      setIsCopied(true);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
 
   return (
     <Screen>
@@ -103,6 +113,13 @@ const Home: NextPage = () => {
             label: "Draw",
             hotKeys: "enter",
             onToggle: () => router.push("/app/editor/sprite"),
+          },
+          {
+            children: "⌘ + C",
+            label: "Copy data",
+            hotKeys: "cmd+c",
+            isActive: isCopied,
+            onToggle: () => copyData(query.d),
           },
         ]}
         action={{ text: "Let me try!", url: "/app/editor/sprite" }}
